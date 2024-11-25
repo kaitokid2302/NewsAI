203d2041916469b9807856fcc1dfee3a54b2523d
mra2322001 <mra2322001>
2024-11-25 16:37:48 +0300

    feat: implement authentication handler and initialize routes

---
+++ internal/handler/auth/auth.go
@@ -0,0 +1,15 @@
+package auth
+
+import "github.com/gin-gonic/gin"
+
+type AuthHandler struct {
+	App *gin.Engine
+}
+
+func NewAuthHandler(app *gin.Engine) *AuthHandler {
+	return &AuthHandler{App: app}
+}
+
+func InitRoute() {
+
+}
