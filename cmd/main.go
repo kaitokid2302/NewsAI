d7c8485b46c116d5cff5b1274457ec8e8ac58457
mra2322001 <mra2322001>
2024-11-29 12:59:53 +0300

    refactor: clean up main function by commenting out unused code and organizing imports

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,22 @@
 package main
 
 import (
-	"fmt"
-
 	_ "github.com/kaitokid2302/NewsAI/docs"
+	"github.com/kaitokid2302/NewsAI/internal/app"
 	"github.com/kaitokid2302/NewsAI/internal/config"
-	"github.com/kaitokid2302/NewsAI/internal/database"
-	"github.com/kaitokid2302/NewsAI/internal/redis"
-	"github.com/kaitokid2302/NewsAI/internal/repository"
-	userservice "github.com/kaitokid2302/NewsAI/internal/service/user"
 )
 
 func main() {
 	config.InitAll()
 
-	// app.Run()
+	app.Run()
 
-	db := database.InitDatabase()
-	redisClient := redis.InitRedis()
-	userRepo := repository.NewUserRepo(db)
-	userService := userservice.NewUserService(userRepo, redisClient)
+	// db := database.InitDatabase()
+	// redisClient := redis.InitRedis()
+	// userRepo := repository.NewUserRepo(db)
+	// userService := userservice.NewUserService(userRepo, redisClient)
 
-	code, er := userService.SendEmail("truonglamthientai321@gmail.com")
-	fmt.Printf("er: %v\n", er)
-	fmt.Printf("code: %v\n", code)
+	// code, er := userService.SendEmail("truonglamthientai321@gmail.com")
+	// fmt.Printf("er: %v\n", er)
+	// fmt.Printf("code: %v\n", code)
 }
